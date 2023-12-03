a68b8c2d9f7ad979d4c18cc85eb04795e52d6d7e
Ada Lamba <barach.8@osu.edu>
2023-12-03 16:04:58 -0500

    finish project
    :

--- mosnet_lime.py
+++ mosnet_lime.py
@@ -1,8 +1,8 @@
 """
-Author: Ada Lamba
+Author: Ada Lamba3.9
 Course: CSE 5539
 Instructor: Khalili
-Version: 12/02/2023
+Version: 12/03/2023
 
 Explaining MOSNet Predictions via LIME Framework:
     In this file, we use the pretrained CNN-BLSTM model from [MOSNet](https://github.com/lochenchou/MOSNet) to predict 
@@ -10,20 +10,18 @@ Explaining MOSNet Predictions via LIME Framework:
 """
 
 import os
-from lime.lime_tabular import LimeTabularExplainer
 from lime.lime_image import LimeImageExplainer
 import model        # MOSNet model definition
 import utils
 import random
 random.seed(1984)   # Set seed to ensure same test/train split as MOSNet original implementation
 import numpy as np
-import time
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 import argparse
-from skimage.segmentation import mark_boundaries
+import pandas as pd
 
 DATA_DIR = './data'
 BIN_DIR = os.path.join(DATA_DIR, 'bin')
@@ -35,9 +33,13 @@ EXP_DIR = os.path.join(DATA_DIR, 'expl')
 
 
 def parse_args():
+    """Parses input arguments
+
+    Returns:
+        argparse.ArgumentParser: contains command line arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--generate_spec', required=False, action='store_true', dest='generate_spec', help='Present if spectrograms should be generated.')
-    parser.add_argument('--tabular', required=False, action='store_true', dest='tabular', help='Present if tabular explainer should be used. Otherwise image explainer will be used.')
     return parser.parse_args()
 
 
@@ -46,41 +48,45 @@ def main():
     
     # preprocess data
     # num_test, num_valid, and batch_size values are defined in the MOSNet train/validation/test split
-    (train_feats, train_labels, test_feats, _) = preprocess(num_test=4000, num_valid=3000, batch_size=64, generate_spec=args.generate_spec)
+    preprocess(num_test=4000, generate_spec=args.generate_spec)
     
     # setup models and explainer
-    mosnet_model, explainer = init(train_feats, train_labels, tabular=args.tabular)
+    mosnet_model, explainer = init()
     
     # explain a data instance
-    expl_idx = np.random.randint(0, test_feats.shape[0])
-    if args.tabular:
-        explain_table(explainer, test_feats, expl_idx, mosnet_model)
-    else:
-        explain_image(explainer, HEATMAP_DIR, mosnet_model)
+    explain_image(explainer, HEATMAP_DIR, mosnet_model)
     
      
-def preprocess(num_test, num_valid, batch_size, generate_spec):
+def preprocess(num_test, generate_spec):
+    """Reads in the input data, performs the train/test split, and returns
+    the features for each input.
+
+    Args:
+        num_test (int): number of data to be in the test set
+        generate_spec (bool): True if spectrogram images should be saved. False otherwise.
+    """
     print('Loading data...', end='', flush=True)
     
     # get list of input audio files, shuffle, and split up by test/train split
     mos_list = utils.read_list(os.path.join(DATA_DIR,'mos_list.txt'))
     random.shuffle(mos_list)
     test_list = mos_list[-num_test:]
-    train_list= mos_list[0:-(num_test+num_valid)]
-
-    # generate features/labels for train and test set
-    (train_feats, train_labels) = get_features(train_list, batch_size)
-    (test_feats, test_labels) = get_features(test_list, batch_size)
     print('Done')
     
     # generate spectrograms
     if generate_spec:
-        generate_spec(mos_list)
-    
-    return (train_feats, train_labels, test_feats, test_labels)
-
+        generate_spectrograms(test_list)
 
 def get_features(data_list, batch_size):
+    """Calls the MOSNet data generator to produce a batch's worth of data. 
+
+    Args:
+        data_list (list): a list of data to generate from
+        batch_size (int): batch size
+
+    Returns:
+        (list, list): a tuple contain the generated data features and labels
+    """
     # get one batch's worth of data from data_list
     gen = utils.data_generator(data_list, BIN_DIR, frame=False, batch_size=batch_size)
     data = next(gen)
@@ -93,16 +99,8 @@ def get_features(data_list, batch_size):
     return (feats, labels)
      
      
-def init(train_feats, train_labels, tabular=False):
+def init():
     """Initializes the MOSNet model and LIME framework.
-
-    Args:
-        train_feats (numpy.ndarray): a 2d numpy array of shape [# data samples, # features]
-                                     where train_feats[i,j] is the jth feature of sample i. 
-        train_labels (numpy.ndarray): a 1d numpy array of shape [# data samples] where 
-                                      train_labels[i] is the true label for sample i. 
-        tabular (bool, optional): False if a LIME tabular explainer should be used and True
-                                  if an image explainer should be used. Defaults to False.
 
     Returns:
         (model, LIME Explainer): the built MOSNet model and chosen LIME explainer object.
@@ -117,41 +115,43 @@ def init(train_feats, train_labels, tabular=False):
 
     # initialize LIME explainer
     print('Initializing LIME explainer...', end='', flush=True)
-    if tabular:
-        explainer = LimeTabularExplainer(training_data=train_feats, training_labels=train_labels, class_names=CLASS_NAMES)
-    else:
-        explainer = LimeImageExplainer()
+    explainer = LimeImageExplainer()
     print('Done')
     
     return mosnet, explainer
 
 
-def explain_table(explainer, test_feats, test_idx, model_obj):
-    print(f'Explaining test instance {test_idx}...', end='', flush=True)
-    tmp = time.time()
-    exp = explainer.explain_instance(test_feats[test_idx], model_obj.flatten_predict)
-    elapsed = (time.time() - tmp)/60.0
-    print(f'Done. Time: {elapsed} minutes.')
-    exp.show_in_notebook(show_table=True, show_all=False)
-
-
 def explain_image(explainer, img_dir, model_obj):
+    """Runs the LIME explainer for all images in a given directory and saves the resulting heatmaps.
+
+    Args:
+        explainer (LimeImageExplainer): the initialized lime image explainer
+        img_dir (str): a directory path containing images to explain
+        model_obj (CNN_BLSTM): the pre-trained MOSNet CNN-BLSTM model
+    """
     print('Generating explanations...')
     images = os.listdir(img_dir)
     images = [os.path.join(HEATMAP_DIR, i) for i in images]
-    im = transform_img_fn([images[0]])[0]
     model_obj.set_instance(images)
-    exp = explainer.explain_instance(im.astype('double'), model_obj.image_predict, hide_color=0, num_samples=10)
+    for img in images:
+        im = transform_img_fn(img)
+        exp = explainer.explain_instance(im.astype('double'), model_obj.image_predict, num_samples=100)
+        true_mos = get_true_mos(img)
+        save_expl_figs(exp, img.split('/')[-1].split('-')[0], true_mos)
+        
     print('Done')
-    true_mos = get_true_mos(images[0], model_obj)
-    save_expl_figs(exp, images[0].split('/')[-1], true_mos)
     
 
 def save_expl_figs(explanation, im_file, true_mos):
-    print('Saving explanations...', end='', flush=True)
-    
+    """Given an explanation, displays the heatmap and saves the file. 
+
+    Args:
+        explanation (LimeImageExplanation): the LIME Image Explanation object.
+        im_file (str): the file path explained in [explanation]
+        true_mos (int): the correct MOS score
+    """
     #Select the same class explained on the figures above.
-    ind =  explanation.top_labels[0]
+    ind =  explanation.top_labels[0] + 1
 
     #Map each explanation weight to the corresponding superpixel
     dict_heatmap = dict(explanation.local_exp[ind])
@@ -161,19 +161,29 @@ def save_expl_figs(explanation, im_file, true_mos):
     plt.imshow(heatmap, cmap = 'RdBu', vmin  = -heatmap.max(), vmax = heatmap.max())
     plt.colorbar()
     plt.title(f'Explanation for {im_file}\nTrue MOS: {true_mos}, Predicted MOS: {ind}')
-    plt.savefig(os.path.join(EXP_DIR, 'expl.png'))
-    print('Done')
+    plt.axis('off')
+    plt.savefig(os.path.join(EXP_DIR, f'{im_file}-expl.png'))
+    plt.close()
 
 
-def get_true_mos(im_path, model_obj):
+def get_true_mos(im_path):
+    """Looks up the true MOS score for a given image file. 
+
+    Args:
+        im_path (str): the file path of the image to get the true MOS for
+
+    Returns:
+        int: the true MOS score for the given file
+    """
     filename = im_path.split('/')[-1].split('-')[0]
-    _feat = utils.read(os.path.join(BIN_DIR,filename+'.h5'))
-    _mag = _feat['mag_sgram']
-    [y_pred, _] = model_obj.model.predict(_mag, verbose=0, batch_size=1)
-    return y_pred[0][0]
+    filename = filename + '.wav'
+    mos_list = pd.read_csv(os.path.join(DATA_DIR, 'mos_list.txt'), sep=',')
+    true_mos = mos_list.loc[mos_list['filename'] == filename, 'mos']
+    
+    return int(np.trunc(float(true_mos)))
 
 
-def generate_spec(file_list, num=10, start=0):
+def generate_spectrograms(file_list, num=10, start=0):
     """Generates magnitude spectrograms and saves the resulting image. 
 
     Args:
@@ -194,28 +204,24 @@ def generate_spec(file_list, num=10, start=0):
         fig, (ax1) = plt.subplots(1, 1)
         librosa.display.specshow(librosa.amplitude_to_db(spec, ref=np.max), y_axis='linear', x_axis='time', sr=sr, ax=ax1)
         plt.axis('off')
-        plt.savefig(os.path.join(HEATMAP_DIR, f'{filename}-spec.png'))
+        plt.savefig(os.path.join(HEATMAP_DIR, f'{filename}-spec.png'), bbox_inches='tight', pad_inches=0.0)
         plt.close(fig)
         
-        # open, add filename as metadata, and save
-        # TODO - I/O inefficient
-        #im = Image.open(os.path.join(HEATMAP_DIR, f'{filename}_spec.png'))
-        #metadata = PngInfo()
-        #metadata.add_text('Source sound file', f'{filename}')
-        #im.save(os.path.join(HEATMAP_DIR, f'{filename}_spec.png'))
-        
-    
     print('Done')
         
 
-def transform_img_fn(path_list):
-    out = []
-    for img_path in path_list:
-        img = image.load_img(img_path, target_size=(299,299))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        out.append(x)
-    return np.vstack(out)
+def transform_img_fn(img_path):
+    """Loads an image and converts it to a numpy array.
+
+    Args:
+        img_path (str): the image file path to load
+
+    Returns:
+        np.ndarray: a 2d numpy array representing the given image
+    """
+    img = image.load_img(img_path)
+    x = image.img_to_array(img)
+    return x
 
 
 if __name__ == '__main__':
